6c7f035a8b89957b4cf947d468d8ff5dfa5e5e9a
121595113@qq.com <121595113@qq.com>
2015-11-29 23:17:33 +0800

    优化配置文件

--- _source/_config/html5-muise.rb
+++ _source/_config/html5-muise.rb
@@ -7,6 +7,7 @@ projectname="html5-muise"
 projectPath="#{projectname}"
 
 http_path = "/"
+additional_import_paths = ["_source/_function"]
 sass_dir = "_source/#{projectname}"
 css_dir = "#{projectPath}/css"
 images_dir = "#{projectPath}/images"
